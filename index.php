fdd01912fff493e2080836ca6727c8823c835a2d
an349ul <54381879+an349ul@users.noreply.github.com>
2019-08-22 11:39:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?
-//echo 'phpのテストです。<br>';
-echo substr($dbinfo);
+echo 'phpのテストです。<br>';
+echo $dbinfo['host'];
+echo substr($dbinfo['path'], 1);
 $dbinfo = parse_url(getenv('DATABASE_URL'));
 $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
