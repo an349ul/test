fefc3ed9bd1c0d63acf54673e22483ab81ecbfbe
an349ul <54381879+an349ul@users.noreply.github.com>
2019-08-22 11:54:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
-echo 'phpのテストです。<br>';
+//echo 'phpのテストです。<br>';
 $dbinfo = parse_url(getenv('DATABASE_URL'));  //DATABASE_URLはHeroku上で定義されている変数。DBの接続情報が含まれている。
 $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
